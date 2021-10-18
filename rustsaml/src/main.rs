2001bd05564c4aebf9268c3329fad65bd2b3c132
Terry Moreland <tsmoreland@live.com>
2021-10-18 20:39:14 +0100

    variation on gosaml using rust - so far just reading from stdin which is more complex than it is in go

---
+++ rustsaml/src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+
+fn main() {
+    let mut inputs: Vec<String>  = Vec::new();
+
+    let mut input = String::new();
+
+    loop {
+        match io::stdin().read_line(&mut input) {
+            Ok(n) => {
+                if n == 0 {
+                    break;
+                } else {
+                    let temp = input.trim();
+                    if temp.is_empty() {
+                       break;
+                    }
+                    inputs.push(String::from(temp));
+                    input = String::new();
+                }
+            },
+            Err(_) => { 
+                break; 
+            }
+        }
+    }
+
+    for input in inputs {
+        println!("{}", input);
+    }
+
+
+}
