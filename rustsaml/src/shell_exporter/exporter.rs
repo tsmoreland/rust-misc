c4b13271dde43b8f414daa96da8576d6ad5b2e6d
Terry Moreland <tsmoreland@live.com>
2021-10-26 13:47:32 +0100

    correct error case, missing type prefix

--- rustsaml/src/shell_exporter/exporter.rs
+++ rustsaml/src/shell_exporter/exporter.rs
@@ -1,10 +1,10 @@
 use crate::shell_exporter::export_error::ExportError;
 use crate::shared::KeyValuePair;
 
-pub fn get_exporter(name: &str) -> Result<fn() -> Result<String, ExportError>, ExportError> {
+pub fn get_exporter(name: &str) -> Result<fn(KeyValuePair) -> Result<String, ExportError>, ExportError> {
     match name.to_uppercase() {
         //"POWERSHELL" => 
-        _ => Err(ExporterNotFound)
+        _ => Err(ExportError::ExporterNotFound)
     }
 
 
