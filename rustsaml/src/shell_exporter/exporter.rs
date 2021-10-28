43d7a0fbae055cbab939134d2e3329fd3ff1e2f8
Terry Moreland <tsmoreland@live.com>
2021-10-28 01:32:24 +0100

    added match statement for POWERSHELL - corrected match input to use .as_str() so it could be compared to raw strings

--- rustsaml/src/shell_exporter/exporter.rs
+++ rustsaml/src/shell_exporter/exporter.rs
@@ -1,9 +1,10 @@
 use crate::shell_exporter::export_error::ExportError;
 use crate::shared::KeyValuePair;
+use crate::shell_exporter::powershell::export_to_powershell;
 
 pub fn get_exporter(name: &str) -> Result<fn(KeyValuePair) -> Result<String, ExportError>, ExportError> {
-    match name.to_uppercase() {
-        //"POWERSHELL" => 
+    match name.to_uppercase().as_str() {
+         "POWERSHELL" => Ok(export_to_powershell),
         _ => Err(ExportError::ExporterNotFound)
     }
 
