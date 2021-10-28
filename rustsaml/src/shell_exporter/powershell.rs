9411c30c3293c73d2766373e6f3c2a5555e0522d
Terry Moreland <tsmoreland@live.com>
2021-10-28 01:31:25 +0100

    added implementation for powershell

---
+++ rustsaml/src/shell_exporter/powershell.rs
@@ -0,0 +1,11 @@
+use crate::shell_exporter::export_error::ExportError;
+use crate::shared::KeyValuePair;
+
+pub fn export_to_powershell(pair: KeyValuePair) -> Result<String, ExportError> {
+    let (key, value) = pair.deconstruct();
+
+    if key.is_empty() {
+        return Err(ExportError::InvalidKeyValuePair);
+    }
+    return Ok(format!("$env:{}='{}'", key, value));
+}
