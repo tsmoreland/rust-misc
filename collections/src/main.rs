ecd9b8ad4d4b0c5dd543fc26af42f617f984b1ce
Terry Moreland <tsmoreland@live.com>
2023-05-14 00:15:07 +0100

    updated main to call set_example

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -2,11 +2,13 @@ use crate::examples::{hashmap_simple_example, primitive_examples, string_example
 use crate::linkedlist_examples::list_example;
 use crate::vec_examples::{vec_general_example, vec_init_example, vecdeque_example};
 use crate::hash_map_examples::hashmap_example;
+use crate::set_examples::set_example;
 
 mod examples;
 mod vec_examples;
 mod linkedlist_examples;
 mod hash_map_examples;
+mod set_examples;
 
 fn main() {
     vector_example();
@@ -27,4 +29,7 @@ fn main() {
 
     println!("--- hashmap example ---");
     hashmap_example();
+
+    println!("--- set example ---");
+    set_example();
 }
