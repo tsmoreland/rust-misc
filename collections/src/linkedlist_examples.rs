66477470acb9617f7b9f986f7b7b6fd6dadf0665
Terry Moreland <tsmoreland@live.com>
2023-05-14 00:46:52 +0100

    resolve warning, mut variable that didn't need mut

--- collections/src/linkedlist_examples.rs
+++ collections/src/linkedlist_examples.rs
@@ -2,13 +2,14 @@ use std::collections::LinkedList;
 
 pub fn list_example() {
     let mut new_list = LinkedList::new();
-    let mut from_list = LinkedList::from([0, 1, 2]);
+    let from_list = LinkedList::from([0, 1, 2]);
 
     new_list.push_front(1);
     new_list.push_front(0);
     new_list.push_back(2);
 
-    if new_list.contains(0) {
+    let zero = 0;
+    if new_list.contains(&zero) {
         println!("new_list contains 0");
     }
 
