b6a081e6a3bf71f6febaee7b5e732a03e07b94b6
Terry Moreland <terry.s.moreland@gmail.com>
2023-05-13 14:32:29 +0100

    vecdeque examples

--- collections/src/vec_examples.rs
+++ collections/src/vec_examples.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 pub fn vec_init_example() {
     let alpha = vec![0, 1, 2, 3, 4]; // initialize with values
     let beta = vec![0; 6]; // initialize with 0 in 6 places
@@ -60,4 +62,49 @@ pub fn vec_general_example() {
 
     alpha.truncate(2);
     println!("alpha truncate: {:?}", alpha);
+}
+
+pub fn vecdeque_example() {
+    let mut new_deque:VecDeque<u32> = VecDeque::new();
+    let mut cap_deque:VecDeque<u32> = VecDeque::with_capacity(5);
+    let mut from_deque = VecDeque::from([0, 1, 2]);
+
+    new_deque.push_front(2);
+    new_deque.push_front(1);
+    new_deque.push_front(0);
+    cap_deque.push_back(0);
+    cap_deque.push_back(1);
+    cap_deque.push_back(2);
+    println!("new: {:?} cap: {:?} from: {:?}", new_deque, cap_deque, from_deque)
+
+    new_deque.pop_front();
+    cap_deque.pop_back();
+    println!("new: {:?} cap: {:?} from: {:?}", new_deque, cap_deque, from_deque)
+
+    if new_deque.contains(1) {
+        println!("new contains 1");
+    }
+
+    let new_front = new_deque.front();
+    let new_mut_front = new_deque.front_mut();
+    match new_front {
+        Some(x) => println!("new_front = {x}"),
+        None => {},
+    }
+    match new_mut_front {
+        Some(&mut x) => println!("new_mut_front = {x}"),
+        None => {},
+    }
+
+    let cap_back = new_deque.back();
+    match cap_back {
+        Some(x) => println!("cap_back = {x}"),
+        None => {},
+    }
+    let cap_mut_back = new_deque.back_mut();
+    match cap_mut_back {
+        Some(x) => println!("cap_mut_back = {x}"),
+        None => {},
+    }
+
 }
