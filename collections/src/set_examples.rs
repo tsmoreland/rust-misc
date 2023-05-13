ffc68fcf414f3d90a73f7f3f420ac7c2715e8586
Terry Moreland <tsmoreland@live.com>
2023-05-14 00:14:55 +0100

    hash set example, btree set is similar but ordered

---
+++ collections/src/set_examples.rs
@@ -0,0 +1,23 @@
+use std::collections::HashSet;
+
+pub fn set_example() {
+    let mut new_set:HashSet<i32> = HashSet::new();
+    new_set.insert(0);
+    new_set.insert(1);
+    new_set.insert(2);
+    new_set.insert(3);
+    new_set.retain(|n| *n < 2);
+
+    let mut from_set = HashSet::from([0, 1, 2]);
+    if new_set.is_subset(&from_set) {
+        println!("new set {:?} is subset of from set {:?}", new_set, from_set)
+    }
+
+    let mut cap_set:HashSet<i32> = HashSet::with_capacity(5);
+    cap_set.insert(2);
+    cap_set.insert(3);
+    cap_set.insert(4);
+
+    let union_set = new_set.union(&cap_set);
+    println!("Union: {:?}", union_set)
+}
